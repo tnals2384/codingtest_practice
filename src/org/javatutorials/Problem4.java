71e6dfe772f0b750321818a636cb01557ad226af
Sumin <tnals2384@gmail.com>
2022-07-07 20:42:18 +0900

    숫자 문자열과 영단어

--- src/org/javatutorials/Problem4.java
+++ src/org/javatutorials/Problem4.java
@@ -1,2 +1,20 @@
-package org.javatutorials;public class Problem4 {
+package org.javatutorials;
+
+import java.util.HashMap;
+
+public class Problem4 {
+    public int solution(String s) {
+        int answer = 0;
+        HashMap<Integer,String> number = new HashMap<>();
+        String[] num = {"zero", "one", "two", "three","four", "five", "six", "seven", "eight", "nine"};
+        for(int i=0; i<10;i++) {
+            number.put(i,num[i]);
+        }
+        for(int i=0;i<10;i++) {
+            s = s.replaceAll(number.get(i),""+i);
+        }
+        answer = Integer.parseInt(s);
+
+        return answer;
+    }
 }
