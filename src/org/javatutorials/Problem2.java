ddfe269b837fe4d752aad5b499c8b5b2f6d32f12
Sumin <tnals2384@gmail.com>
2022-07-07 20:40:33 +0900

    신고 결과 받기

--- src/org/javatutorials/Problem2.java
+++ src/org/javatutorials/Problem2.java
@@ -1,2 +1,29 @@
-package org.javatutorials;public class Problem2 {
+package org.javatutorials;
+import java.util.*;
+public class Problem2 {
+    public int[] solution(String[] id_list, String[] report, int k) {
+        int[] answer = new int[id_list.length];
+        Map<String,HashSet<String>> map = new HashMap<>();
+        Map<String,Integer> count = new HashMap<>();
+
+        for(int i=0;i<id_list.length;i++) {
+            map.put(id_list[i],new HashSet<>());
+            count.put(id_list[i],i);
+        }
+        for (String e: report) {
+            String[] id = e.split(" ");
+            map.get(id[1]).add(id[0]);
+        }
+
+        for (int i=0;i<id_list.length;i++) {
+            HashSet<String> rep = map.get(id_list[i]);
+            if (rep.size() >= k) {
+                for(String name: rep) {
+                    answer[count.get(name)]++;
+                }
+            }
+        }
+        return answer;
+
+    }
 }
